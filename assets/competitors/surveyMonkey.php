18c5e43891294fd4404691e1e97b66b929f145dd
teobot <theoclapperton@outlook.com>
2019-11-08 19:53:04 +0000

    included placeholder sections

--- assets/competitors/surveyMonkey.php
+++ assets/competitors/surveyMonkey.php
@@ -2,37 +2,248 @@
 	Survey Monkey
 </h2>
 
-<div class="card mb-3">
-    <img src="assets/img/googleFormsLayoutPres.PNG" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">
-        Layout/presentation of surveys
-        </h5>
-        <p class="card-text">
-            PLACEHOLDER
-
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Positives</th>
-                        <th scope="col">Negatives</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>PLACEHOLDER.</td>
-                        <td>PLACEHOLDER.</td>
-                    </tr>
-                    <tr>
-                        <td>PLACEHOLDER.</td>
-                        <td>PLACEHOLDER.</td>
-                    </tr>
-                    <tr>
-                        <td>PLACEHOLDER.</td>
-                        <td>PLACEHOLDER.</td>
-                    </tr>
-                </tbody>
-            </table>
-        </p>
+<?php // PLACEHOLDER section Card ?>
+<div class="container" style="background-color: paleturquoise; border-left: 6px solid mediumturquoise;">    
+    <br>
+    <div class="row">
+        <div class="col">
+            <h4>PLACEHOLDERTITLE</h4>
+            <p class="text-center">
+                PLACEHOLDERTEXT
+            </p>  
+        </div>
     </div>
+    <div class="row">
+        <div class="text-center">
+                <img src="assets/img/googleFormsLayout.PNG" class="rounded img-fluid" alt="Responsive image">
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col">
+            <ul class="list-group">
+                <li class="list-group-item"><h5 class="mb-1">Positives</h5></li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>  
+            </ul>
+        </div>
+        <div class="col">
+            <ul class="list-group">
+                <li class="list-group-item"><h5 class="mb-1">Negatives</h5></li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li> 
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>             
+            </ul>
+        </div>
+    </div>
+    <hr>
 </div>
+<?php // END PLACEHOLDER section Card END ?>
+
+<?php // PLACEHOLDER section Card ?>
+<div class="container" style="background-color: paleturquoise; border-left: 6px solid mediumturquoise;">    
+    <br>
+    <div class="row">
+        <div class="col">
+            <h4>PLACEHOLDERTITLE</h4>
+            <p class="text-center">
+                PLACEHOLDERTEXT
+            </p>  
+        </div>
+    </div>
+    <div class="row">
+        <div class="text-center">
+                <img src="assets/img/googleFormsLayout.PNG" class="rounded img-fluid" alt="Responsive image">
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col">
+            <ul class="list-group">
+                <li class="list-group-item"><h5 class="mb-1">Positives</h5></li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>  
+            </ul>
+        </div>
+        <div class="col">
+            <ul class="list-group">
+                <li class="list-group-item"><h5 class="mb-1">Negatives</h5></li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li> 
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>             
+            </ul>
+        </div>
+    </div>
+    <hr>
+</div>
+<?php // END PLACEHOLDER section Card END ?>
+
+<?php // PLACEHOLDER section Card ?>
+<div class="container" style="background-color: paleturquoise; border-left: 6px solid mediumturquoise;">    
+    <br>
+    <div class="row">
+        <div class="col">
+            <h4>PLACEHOLDERTITLE</h4>
+            <p class="text-center">
+                PLACEHOLDERTEXT
+            </p>  
+        </div>
+    </div>
+    <div class="row">
+        <div class="text-center">
+                <img src="assets/img/googleFormsLayout.PNG" class="rounded img-fluid" alt="Responsive image">
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col">
+            <ul class="list-group">
+                <li class="list-group-item"><h5 class="mb-1">Positives</h5></li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>  
+            </ul>
+        </div>
+        <div class="col">
+            <ul class="list-group">
+                <li class="list-group-item"><h5 class="mb-1">Negatives</h5></li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li> 
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>             
+            </ul>
+        </div>
+    </div>
+    <hr>
+</div>
+<?php // END PLACEHOLDER section Card END ?>
+
+<?php // PLACEHOLDER section Card ?>
+<div class="container" style="background-color: paleturquoise; border-left: 6px solid mediumturquoise;">    
+    <br>
+    <div class="row">
+        <div class="col">
+            <h4>PLACEHOLDERTITLE</h4>
+            <p class="text-center">
+                PLACEHOLDERTEXT
+            </p>  
+        </div>
+    </div>
+    <div class="row">
+        <div class="text-center">
+                <img src="assets/img/googleFormsLayout.PNG" class="rounded img-fluid" alt="Responsive image">
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col">
+            <ul class="list-group">
+                <li class="list-group-item"><h5 class="mb-1">Positives</h5></li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>  
+            </ul>
+        </div>
+        <div class="col">
+            <ul class="list-group">
+                <li class="list-group-item"><h5 class="mb-1">Negatives</h5></li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li> 
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>             
+            </ul>
+        </div>
+    </div>
+    <hr>
+</div>
+<?php // END PLACEHOLDER section Card END ?>
+
+<?php // PLACEHOLDER section Card ?>
+<div class="container" style="background-color: paleturquoise; border-left: 6px solid mediumturquoise;">    
+    <br>
+    <div class="row">
+        <div class="col">
+            <h4>PLACEHOLDERTITLE</h4>
+            <p class="text-center">
+                PLACEHOLDERTEXT
+            </p>  
+        </div>
+    </div>
+    <div class="row">
+        <div class="text-center">
+                <img src="assets/img/googleFormsLayout.PNG" class="rounded img-fluid" alt="Responsive image">
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col">
+            <ul class="list-group">
+                <li class="list-group-item"><h5 class="mb-1">Positives</h5></li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>  
+            </ul>
+        </div>
+        <div class="col">
+            <ul class="list-group">
+                <li class="list-group-item"><h5 class="mb-1">Negatives</h5></li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li> 
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>             
+            </ul>
+        </div>
+    </div>
+    <hr>
+</div>
+<?php // END PLACEHOLDER section Card END ?>
+
+<?php // PLACEHOLDER section Card ?>
+<div class="container" style="background-color: paleturquoise; border-left: 6px solid mediumturquoise;">    
+    <br>
+    <div class="row">
+        <div class="col">
+            <h4>PLACEHOLDERTITLE</h4>
+            <p class="text-center">
+                PLACEHOLDERTEXT
+            </p>  
+        </div>
+    </div>
+    <div class="row">
+        <div class="text-center">
+                <img src="assets/img/googleFormsLayout.PNG" class="rounded img-fluid" alt="Responsive image">
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col">
+            <ul class="list-group">
+                <li class="list-group-item"><h5 class="mb-1">Positives</h5></li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>  
+            </ul>
+        </div>
+        <div class="col">
+            <ul class="list-group">
+                <li class="list-group-item"><h5 class="mb-1">Negatives</h5></li>
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li> 
+                <li class="list-group-item">PLACEHOLDER</li>
+                <li class="list-group-item">PLACEHOLDER</li>             
+            </ul>
+        </div>
+    </div>
+    <hr>
+</div>
+<?php // END PLACEHOLDER section Card END ?>
