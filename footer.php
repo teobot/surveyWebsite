8cf104e23f1e72ca3c30dfe04d06440c5748dea7
teobot <theoclapperton@outlook.com>
2019-11-11 00:16:52 +0000

    removed the main.js from each page

--- footer.php
+++ footer.php
@@ -12,10 +12,9 @@ echo <<<_END
     <br>
     	&copy;6G5Z2107 - Created By: $student_name - StudentNumber: $student_id - 2019/20
     	
-		<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>	
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>	
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-		<script src="assets/javascript/main.js"></script>
 			</div>
 		</body>
     </html>
