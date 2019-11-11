acdbe1c1106bdd415403caa521f914612432656b
Theo Clapperton <theoclapperton@outlook.com>
2019-11-11 12:34:59 +0000

    included section 1 and 2

--- assets/competitors/surveyMonkey.php
+++ assets/competitors/surveyMonkey.php
@@ -2,14 +2,18 @@
 	Survey Monkey
 </h2>
 
-<?php // PLACEHOLDER section Card ?>
+<?php // 1. Layout/presentation of surveys section Card ?>
 <div class="container" style="background-color: paleturquoise; border-left: 6px solid mediumturquoise;">    
     <br>
     <div class="row">
         <div class="col">
-            <h4>PLACEHOLDERTITLE</h4>
+            <h4>1. Layout/presentation of surveys</h4>
             <p class="text-center">
-                PLACEHOLDERTEXT
+		The layout of the survey is sleek and simple, It includes a progress bar at the 
+		bottom so that users can see how much is left or how far they have come. The page layout 
+	    	however isn't centered, and they is no way of aligning the questions to one side. Themes are not 
+	    	free like google forms, meaning the green theme is default. There is also a 'powered by' footer 
+	    	which can't be removed without paying.
             </p>  
         </div>
     </div>
@@ -23,34 +27,40 @@
         <div class="col">
             <ul class="list-group">
                 <li class="list-group-item"><h5 class="mb-1">Positives</h5></li>
-                <li class="list-group-item">PLACEHOLDER</li>
-                <li class="list-group-item">PLACEHOLDER</li>
-                <li class="list-group-item">PLACEHOLDER</li>
-                <li class="list-group-item">PLACEHOLDER</li>  
+                <li class="list-group-item">Progress bar shows the users how far through they are.</li>
+                <li class="list-group-item">Design is very smooth and simple.</li>
+                <li class="list-group-item">Font is clear and recognisable.</li>
             </ul>
         </div>
         <div class="col">
             <ul class="list-group">
                 <li class="list-group-item"><h5 class="mb-1">Negatives</h5></li>
-                <li class="list-group-item">PLACEHOLDER</li>
-                <li class="list-group-item">PLACEHOLDER</li> 
-                <li class="list-group-item">PLACEHOLDER</li>
-                <li class="list-group-item">PLACEHOLDER</li>             
+                <li class="list-group-item">All the questions are aligned left but buttons are centered.</li>
+                <li class="list-group-item">Without paying the survey has a medium sized 'powered by' at the bottom.</li> 
+                <li class="list-group-item">Without paying you cannot change the theme colour.</li>           
             </ul>
         </div>
     </div>
     <hr>
 </div>
-<?php // END PLACEHOLDER section Card END ?>
+<?php // END Layout/presentation of surveys section Card END ?>
 
-<?php // PLACEHOLDER section Card ?>
+<?php // 2. Ease of Use of surveys section Card ?>
 <div class="container" style="background-color: paleturquoise; border-left: 6px solid mediumturquoise;">    
     <br>
     <div class="row">
         <div class="col">
-            <h4>PLACEHOLDERTITLE</h4>
+            <h4>2. Ease of Use</h4>
             <p class="text-center">
-                PLACEHOLDERTEXT
+                The survey once finished is very easy to use, However I've found with a comment box, there is a confusing 
+	    	'OK' box that appears when the user enters text into the area, This button doesn't appear to be needed as clicking 
+	    	'DONE' submits the users response anyway, but the 'OK' button appears to save the users current input. I believe this will confuse 
+	    	the form taker. The creation of surveys is slightly confusing to a first user with a steep learning curve, however 
+	    	the website deploys mulitple methods such as tutorials and introductions aswell as different survey creation methods 
+	    	such as pre-made surveys, question examples and a simplified survey creation where the website does it for you after 
+	    	the users answers some simple questions. The websites also includes many 'microtransaction landmines' where clicking on 
+	    	a feature will add addititional cost if the user wants to publish the survey, the user must then go back through the 
+	    	survey and removed all these features if they want it to be free.
             </p>  
         </div>
     </div>
@@ -64,25 +74,23 @@
         <div class="col">
             <ul class="list-group">
                 <li class="list-group-item"><h5 class="mb-1">Positives</h5></li>
-                <li class="list-group-item">PLACEHOLDER</li>
-                <li class="list-group-item">PLACEHOLDER</li>
-                <li class="list-group-item">PLACEHOLDER</li>
-                <li class="list-group-item">PLACEHOLDER</li>  
+                <li class="list-group-item">Pre-made questions.</li>
+                <li class="list-group-item">Website will create a survey based on users answers to questions.</li>
+                <li class="list-group-item">Tutorials and introductions.</li>
             </ul>
         </div>
         <div class="col">
             <ul class="list-group">
                 <li class="list-group-item"><h5 class="mb-1">Negatives</h5></li>
-                <li class="list-group-item">PLACEHOLDER</li>
-                <li class="list-group-item">PLACEHOLDER</li> 
-                <li class="list-group-item">PLACEHOLDER</li>
-                <li class="list-group-item">PLACEHOLDER</li>             
+                <li class="list-group-item">Steep learning curve.</li>
+                <li class="list-group-item">lot's of paid features.</li> 
+                <li class="list-group-item">Complex and confusing layout.</li>
             </ul>
         </div>
     </div>
     <hr>
 </div>
-<?php // END PLACEHOLDER section Card END ?>
+<?php // END Ease of Use of surveys section Card END ?>
 
 <?php // PLACEHOLDER section Card ?>
 <div class="container" style="background-color: paleturquoise; border-left: 6px solid mediumturquoise;">    
