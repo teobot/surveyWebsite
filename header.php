39966f40e7ebcf0f2598ebbdf5eab2a79f4fe4e1
b0nfire <theoclapperton@outlook.com>
2019-11-13 12:12:05 +0000

    removed the stylesheet

--- header.php
+++ header.php
@@ -4,9 +4,7 @@ echo<<<_END
 <html>
 	<head>
 		<title>A Survey Website</title>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link rel="stylesheet" type="text/css" href="assets/style/mystyle.css">
-		<noscript><div class="p-3 mb-2 bg-danger text-white">Sorry, your browser does not support JavaScript!</div></noscript>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">		<noscript><div class="p-3 mb-2 bg-danger text-white">Sorry, your browser does not support JavaScript!</div></noscript>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	</head>
 		<div class="container-fluid">
