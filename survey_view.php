8178b8408867a6f0739040340295cb9bb69efa35
teobot <theoclapperton@outlook.com>
2019-11-14 00:47:59 +0000

    added a script so the user can upload the survey

--- survey_view.php
+++ survey_view.php
@@ -4,11 +4,17 @@ require_once "header.php";
 
 $surveyID = $_GET['surveyID'];
 
+echo<<<_END
+    <div class="alert alert-danger" id="errorMessage" style="display: none;">
+        <strong>Error!</strong> Please make sure all data is correct and inputted!
+    </div>
+_END;
+
 echo '<div class="text-center">';
 
 echo '<div><h1 id="surveyTitle">Loading...</h1></div>';
 
-echo '<div id="questionsContainer">Loading...</div>';
+echo '<div id="questionsContainer" class="col-md-8 offset-md-2" >Loading...</div>';
 
 echo '</div>';
 
@@ -34,20 +40,47 @@ echo<<<_END
                     $('#surveyTitle').html(data.survey_title);
 
                     $.each(data.survey_JSON, function( key, value ) {
-                        $('#questionsContainer').append("<div class='questions'> <div class='h6'>"+ value.title +"</div> <div><small>"+value.label+"</small></div> <div class='input'><input></input></div> </div><br>");
+                        $('#questionsContainer').append("<form class='card'> <div class='h6'>"+ value.title +"</div> <div><small>"+value.label+"</small></div> <div class='input'><input name='questionAnswer' type='"+value.inputType+"'></input></div> </form><br>");
                       });
 
-                      $('#questionsContainer').append("<div><button>Submit</button><div>");
+                      $('#questionsContainer').append("<div><button id='submitSurvey' type='button'>Submit</button><div>");
 
                 })
                 .fail(function(jqXHR) {
                     // debug message to help during development:
                     console.log('request returned failure, HTTP status code ' + jqXHR.status);
-                })
-                    
-                // call this function again after a brief pause:    
+                })   
         }
-    </script>
+
+  $(document).ready(function(){
+
+    // If the user clicks submit this function handles getting all the data back
+    $(document).on('click', '#submitSurvey', function(){
+
+        // Create a new array full of our created questions.
+            var surveyData = [];
+
+        // Each question is actually a form and by using this function below it finds all the forms,
+        // And then using .serializeArray() it gets all the values out and places them in a 
+        // Indexed javascript array ready to be sent to the submission page to be encode to a JSON array and inserted into the db
+            $("form").each(function(){
+                surveyData.push($( this ).serializeArray());
+            });
+
+        //Now we post this array to a API that looks to see if the data is valid
+            $.post('assets/api/insertResponse.php', {survey_RESPONSE: surveyData, surveyID: $surveyID})
+            .done(function(data) {
+                //REPLACE WITH A THANK YOU FOR SUBMITTING MESSAGE
+                //window.location.replace("index.php");
+                console.log(data);
+            })
+            .fail(function() {
+              document.getElementById("errorMessage").style.display= 'block';
+            })        
+    });
+
+  });
+</script>
 _END;
 
 // finish off the HTML for this page:
