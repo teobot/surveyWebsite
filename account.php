e06b0354cb37c1da548ea474434deb8cb2974ae7
teobot <theoclapperton@outlook.com>
2019-11-17 18:14:57 +0000

    move the account edit to the account page

--- account.php
+++ account.php
@@ -6,18 +6,124 @@
 // execute the header script:
 require_once "header.php";
 
+$password = "";
+$firstname = "";
+$surname = "";
+$telephoneNumber = "";
+$dateOfBirth = "";
+$email = "";
+
+$password_err = "";
+$firstname_err = "";
+$surname_err = "";
+$telephoneNumber_err = "";
+$dateOfBirth_err = "";
+$email_err = "";
+
+$message_err = "";
+
+$show_account_form = false;
 
 // checks the session variable named 'loggedIn'
 // take note that of the '!' (NOT operator) that precedes the 'isset' function
 if (!isset($_SESSION['loggedIn']))
 {
 	// user isn't logged in, display a message saying they must be:
-	echo "You must be logged in to view this page.<br>";
+	echo "<div class='col-md-6 offset-md-3 text-center'><div class='alert alert-success' role='alert'>You are already logged in, please log out first.</div></div>";
 }
 
+elseif ( (isset($_POST['password'])) || (isset($_POST['firstname'])) || (isset($_POST['surname'])) || (isset($_POST['telephoneNumber'])) || (isset($_POST['dateOfBirth'])) || (isset($_POST['email'])) )
+{
+	// user just tried to update their profile
+	$username = $_SESSION["username"];
+	
+	// connect directly to our database (notice 4th argument) we need the connection for sanitisation:
+	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	
+	// if the connection fails, we need to know, so allow this exit:
+	if (!$connection)
+	{
+		die("Connection failed: " . $mysqli_connect_error);
+	}
+
+	$password = $_POST['password'];
+	$firstname = $_POST['firstname'];
+	$surname = $_POST['surname'];
+	$telephoneNumber = $_POST['telephoneNumber'];
+	$dateOfBirth = $_POST['dateOfBirth'];
+	$email = $_POST['email'];
+
+	// SANITISATION CODE MISSING:
+    // ...
+	$password = sanitise($password, $connection);
+	$firstname = sanitise($firstname, $connection);
+	$surname = sanitise($surname, $connection);
+	$telephoneNumber = sanitise($telephoneNumber, $connection);
+	$dateOfBirth = sanitise($dateOfBirth, $connection);
+	$email = sanitise($email, $connection);
+	// ...
+	
+	// SERVER-SIDE VALIDATION CODE MISSING:
+    // ...
+	$password_err = validateString($password, 1, 16, "Password");
+	$firstname_err = validateString($firstname, 1, 64, "Firstname");
+	$surname_err = validateString($surname, 1, 32, "Surname");
+	$telephoneNumber_err = validateTelephoneNumber($telephoneNumber, 9);
+	$dateOfBirth_err = validateDate($dateOfBirth);
+	$email_err = validateEmail($email, 1, 64, "Email");
+    // ...
+	
+	$message_err = $password_err . $firstname_err . $surname_err . $telephoneNumber_err . $dateOfBirth_err . $email_err;
+	
+	// check that all the validation tests passed before going to the database:
+	if ($message_err == "")
+	{		
+		// read their username from the session:
+		$username = $_SESSION["username"];
+		
+		// check to see if this user already had a favourite:
+		$query = "SELECT `username`, `password`, `firstname`, `surname`, `email`, `dob`, `telephoneNumber` FROM `users` WHERE username='$username'";
+		
+		// this query can return data ($result is an identifier):
+		$result = mysqli_query($connection, $query);
+		
+		// how many rows came back? (can only be 1 or 0 because username is the primary key in our table):
+		$n = mysqli_num_rows($result);
+			
+		// if there was a match then UPDATE their profile data, otherwise INSERT it:
+		if ($n > 0)
+		{
+			// we need an UPDATE:
+			$query = "UPDATE `users` SET `password` = '$password', `firstname` = '$firstname', `surname` = '$surname', `email` = '$email', `dob` = '$dateOfBirth', `telephoneNumber` = '07$telephoneNumber' WHERE `users`.`username` = '$username'";
+			$result = mysqli_query($connection, $query);		
+		}
+	
+
+		// no data returned, we just test for true(success)/false(failure):
+		if ($result) 
+		{
+			// show a successful update message:
+			$message_err = '<div class="alert alert-success" role="alert"><h4 class="alert-heading">Account Information Updated!</h4><p>You have successfully update your account, Go back to the <a href="account.php">account!</a></p></div>';
+		}
+		else
+		{
+			// show an unsuccessful update message:
+			$message_err = '<div class="alert alert-danger" role="alert"><h4 class="alert-heading">Account Update Unsuccessful!</h4><p>Something went wrong with the update!, Please Go back to the <a href="account.php">account</a> and try again!</p></div>';
+		}
+	}
+	else
+	{
+		// validation failed, show the form again with guidance:
+		$show_account_form = true;
+	}
+	
+	// we're finished with the database, close the connection:
+	mysqli_close($connection);
+
+}
 // the user must be signed-in, show them suitable page content
 else
-{    
+{
     // user is already logged in, read their username from the session:
 	$username = $_SESSION["username"];
 	
@@ -32,76 +138,102 @@ else
 	}
 	
 	// check for a row in our profiles table with a matching username:
-	$query = "SELECT * FROM users WHERE username='$username'";
+	$query = "SELECT `username`, `password`, `firstname`, `surname`, `email`, `dob`, `telephoneNumber` FROM `users` WHERE username='$username'";
 	
 	// this query can return data ($result is an identifier):
 	$result = mysqli_query($connection, $query);
 	
 	// how many rows came back? (can only be 1 or 0 because username is the primary key in our table):
 	$n = mysqli_num_rows($result);
-		
-	// if there was a match then extract their profile data:
+
 	if ($n > 0)
 	{
 		// use the identifier to fetch one row as an associative array (elements named after columns):
 		$row = mysqli_fetch_assoc($result);
-		// display their profile data:
-		echo<<<_END
-		<div class="container">
-			<h4>Account Information</h4>
-			<small>Here you can view/edit account information. Update information <a href="account_set.php">here.</a></small>
-			<hr>
-			<div class="container">
-				<div class="row">
-					<div class="col">
-						<label><p class="h6">Username:</p></label>
-						<input type="text" class="form-control" placeholder="{$row['username']}" aria-label="username" aria-describedby="addon-wrapping">
-					</div>
-					<div class="col">
-						<label><p class="h6">Password:</p></label>
-						<input type="text" class="form-control" placeholder="{$row['password']}" aria-label="password" aria-describedby="addon-wrapping">
-					</div>
-				</div><hr>
-				<div class="row">
-					<div class="col">
-						<label><p class="h6">Firstname:</p></label>
-						<input type="text" class="form-control" placeholder="{$row['firstname']}" aria-label="Firstname" aria-describedby="addon-wrapping">
-					</div>
-					<div class="col">
-						<label><p class="h6">Surname:</p></label>
-						<input type="text" class="form-control" placeholder="{$row['surname']}" aria-label="Surname" aria-describedby="addon-wrapping">
-					</div>
-				</div><br>
-				<div class="row">
-					<div class="col">
-						<label><p class="h6">Telephone number:</p></label>
-						<input type="text" class="form-control" placeholder="{$row['telephoneNumber']}" aria-label="telephone" aria-describedby="addon-wrapping">
-					</div>
-					<div class="col">
-						<label><p class="h6">Date of birth:</p></label>
-						<input type="text" class="form-control" placeholder="{$row['dob']}" aria-label="dob" aria-describedby="addon-wrapping">
-					</div>
-				</div><br>
-				<div class="row">
-					<div class="col">
-						<label><p class="h6">Email:</p></label>
-						<input type="text" class="form-control" placeholder="{$row['email']}" aria-label="email" aria-describedby="addon-wrapping">
-					</div>
-				</div>
-			</div>
-		</div>
-_END;
+		// extract their profile data for use in the HTML:
+		$password = $row['password'];
+		$firstname = $row['firstname'];
+		$surname = $row['surname'];
+		$telephoneNumber = substr($row['telephoneNumber'], 2);
+		$dateOfBirth = $row['dob'];
+		$email = $row['email'];
 	}
 
-	else
-	{
-		// no match found, prompt user to set up their profile:
-		echo "You still need to set up a profile!<br>";
-	}
+	// show the set profile form:
+	$show_account_form = true;
 	
 	// we're finished with the database, close the connection:
 	mysqli_close($connection);
+}
 		
+	// if there was a match then extract their profile data:
+
+echo $message_err;
+if ($show_account_form)	
+{
+// display their profile data:
+echo<<<_END
+	<div class="container">
+
+		<h4>Account Information</h4>
+		<small>Here you can view/edit account information. Simply edit the information and click Update!</small>
+		<hr>
+		<form class="container" method="post">
+			<div class="row">
+				<div class="col">
+					<label><p class="h6">Username:</p></label>
+					<input type="text" class="form-control" placeholder="$username" disabled>
+				</div>
+				<div class="col">
+					<label><p class="h6">Password:</p></label>
+					<input type="text" name="password" class="form-control" value="$password">
+				</div>
+			</div><hr>
+			<div class="row">
+				<div class="col">
+					<label><p class="h6">Firstname:</p></label>
+					<input type="text" name="firstname" class="form-control" value="$firstname">
+				</div>
+				<div class="col">
+					<label><p class="h6">Surname:</p></label>
+					<input type="text" name="surname" class="form-control" value="$surname">
+				</div>
+			</div><br>
+			<div class="row">
+				<div class="col">
+					<label><p class="h6">Telephone number:</p></label>
+					<div class="input-group mb-2">
+						<div class="input-group-prepend">
+							<div class="input-group-text">07</div>
+						</div>
+						<input type="text" name="telephoneNumber" class="form-control" value="$telephoneNumber">
+					</div>		
+				</div>
+				<div class="col">
+					<label><p class="h6">Date of birth:</p></label>
+					<input type="date" name="dateOfBirth" class="form-control" value="$dateOfBirth">
+				</div>
+			</div><br>
+			<div class="row">
+				<div class="col">
+					<label><p class="h6">Email:</p></label>
+					<input type="email" name="email" class="form-control" value="$email">
+				</div>
+			</div>
+
+			<hr>
+
+			<div class="row">
+				<div class="col">
+					<button style="float: right;" type="submit" value="Submit" class="btn btn-primary btn-lg">Update</button>
+				</div>
+			</div>
+			
+			<br>
+
+		</form>
+	</div>
+_END;
 }
 
 // finish off the HTML for this page:
