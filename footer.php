4c1f506f358d5f7ea71a5590d3460c88c230dc22
teobot <theoclapperton@outlook.com>
2019-11-22 01:16:42 +0000

    minor style change

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@ $student_id = "18055445";
 
 echo <<<_END
     <br>
-    	&copy;6G5Z2107 - Created By: $student_name - StudentNumber: $student_id - 2019/20
+		&copy;6G5Z2107 - Created By: $student_name - StudentNumber: $student_id - 2019/20
     	
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>	
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
