02455f8e86d85c7da1d8d76710c6fd98d630a1d7
teobot <theoclapperton@outlook.com>
2019-12-11 22:04:34 +0000

    started the conclusion

--- assets/competitors/conclusion.php
+++ assets/competitors/conclusion.php
@@ -1,1 +1,47 @@
-<h1 class="display">OVERALL</h1>
+<h2 class="text-center">
+    Conclusion
+</h2>
+
+<div class="container" style="background-color: whitesmoke; border-left: 6px solid lightslategray;">
+    <br>
+    <div class="row">
+        <div class="col">
+            <h4>What did I learn from my research!</h4>
+            <p class="text-left">
+                From my research I've learnt the best way to display response information is using graphs for repetitive data.<br>
+                I really like the google forms design, they have a "card" design, for each question and response, I feel that this is gives a simple and clean feel. <br>
+                The amount of questions 
+            </p>  
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col">
+            <ul class="list-group">
+            <li class="list-group-item list-group-item-success"><h5 class="mb-1"> <small>Features I</small> <strong>will</strong> <small>include from my research!</small></h5></li>
+                <li class="list-group-item">Give the user the ability to create as many questions as they want.</li>
+                <li class="list-group-item">As many responses as the user wants, no limit.</li>
+                <li class="list-group-item">placeholder</li>
+                <li class="list-group-item">placeholder</li>
+                <li class="list-group-item">placeholder</li>
+                <li class="list-group-item">placeholder</li>
+                <li class="list-group-item">placeholder</li>
+                <li class="list-group-item">placeholder</li>
+            </ul>
+        </div>
+        <div class="col">
+            <ul class="list-group">
+                <li class="list-group-item list-group-item-warning"><h5 class="mb-1"> <small>Features I</small> <strong>will not</strong> <small>include from my research!</small></h5></li>
+                <li class="list-group-item">Any features that a user has to pay for.</li>
+                <li class="list-group-item">A footer or header that promotes the website.</li>
+                <li class="list-group-item">placeholder</li>
+                <li class="list-group-item">placeholder</li>
+                <li class="list-group-item">placeholder</li>
+                <li class="list-group-item">placeholder</li>
+                <li class="list-group-item">placeholder</li>
+                <li class="list-group-item">placeholder</li>
+            </ul>
+        </div>
+    </div>
+    <hr>
+</div>
