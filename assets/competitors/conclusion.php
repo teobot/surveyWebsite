94ffc2af85bea09624c78a2143978251112dc0cd
teobot <theoclapperton@outlook.com>
2019-12-12 23:48:59 +0000

    finished the conclusion

--- assets/competitors/conclusion.php
+++ assets/competitors/conclusion.php
@@ -7,10 +7,20 @@
     <div class="row">
         <div class="col">
             <h4>What did I learn from my research!</h4>
-            <p class="text-left">
-                From my research I've learnt the best way to display response information is using graphs for repetitive data.<br>
-                I really like the google forms design, they have a "card" design, for each question and response, I feel that this is gives a simple and clean feel. <br>
-                The amount of questions 
+            <p class="text-left"> <hr>
+                From my research I've learnt the best way to display response information is using graphs for repetitive data, For this I will use google charts.<hr>
+                I really like the google forms design, they have a "card" design, for each question and response, I feel that this is gives a simple and clean feel. Following this
+                I will use Bootstrap to style my website, This will provide interactive and responsive elements with rapid prototyping.<hr>
+                All the websites didn't restrict the number of questions that a user can have on a survey, So I'm going to create a modular way of adding and removing questions to a survey.<hr>
+                Zoho limited the number of responses in front of a paywall, to get access to the rest you would need to pay. My website will include no such restriction and allow unlimited number of responses <hr>
+                All websites have responsive animation on checkboxes and input areas, this provides useful data on whether a input has be inputted. <hr>
+                All the websites i've researched allow the survey to be given to anyone. I will create the survey page to be allowed to be sent to anyone to complete, Even without a account. <hr>
+                Google forms is super easy to add new questions, I want to have different easy to read and click buttons to add new questions. <hr>
+                All these websites have complex survey IDs, I feel these can be difficult to enter and copy. I want simple IDs so that users can find them easily. <hr>
+                Zoho has this trial feature where after the 7 days features are disabled. This is highly annoying and I feel will turn users away if used, I won't be using these. <hr>
+                All these websites give the user the ability to view each response as well as all the data, I will allow the user to view each response for more personal research. <hr>
+                Survey Monkey uses the final submission page as a place to show ads, This is annoying and low quality. I want the website to remain high quality so will not include any ads. <hr>
+
             </p>  
         </div>
     </div>
@@ -21,12 +31,12 @@
             <li class="list-group-item list-group-item-success"><h5 class="mb-1"> <small>Features I</small> <strong>will</strong> <small>include from my research!</small></h5></li>
                 <li class="list-group-item">Give the user the ability to create as many questions as they want.</li>
                 <li class="list-group-item">As many responses as the user wants, no limit.</li>
-                <li class="list-group-item">placeholder</li>
-                <li class="list-group-item">placeholder</li>
-                <li class="list-group-item">placeholder</li>
-                <li class="list-group-item">placeholder</li>
-                <li class="list-group-item">placeholder</li>
-                <li class="list-group-item">placeholder</li>
+                <li class="list-group-item">Have the design like google forms, In a card based question.</li>
+                <li class="list-group-item">Have the questions laid out in a simple manner.</li>
+                <li class="list-group-item">Use Google Charts to display charts to help show useful information.</li>
+                <li class="list-group-item">Have smooth animation on the different types of input areas.</li>
+                <li class="list-group-item">Allow the survey to sent to anyone without a account.</li>
+                <li class="list-group-item">Allow the creator to view each response as well as the wide data.</li>
             </ul>
         </div>
         <div class="col">
@@ -34,14 +44,41 @@
                 <li class="list-group-item list-group-item-warning"><h5 class="mb-1"> <small>Features I</small> <strong>will not</strong> <small>include from my research!</small></h5></li>
                 <li class="list-group-item">Any features that a user has to pay for.</li>
                 <li class="list-group-item">A footer or header that promotes the website.</li>
-                <li class="list-group-item">placeholder</li>
-                <li class="list-group-item">placeholder</li>
-                <li class="list-group-item">placeholder</li>
-                <li class="list-group-item">placeholder</li>
-                <li class="list-group-item">placeholder</li>
-                <li class="list-group-item">placeholder</li>
+                <li class="list-group-item">The ability to skip questions, This makes the question redundant if the user skips it.</li>
+                <li class="list-group-item">Complex survey IDs, these can be difficult to copy and enter.</li>
+                <li class="list-group-item">Have a "trial" and after restrict the users features.</li>
+                <li class="list-group-item">Have any Advertisements on the website.</li>
             </ul>
         </div>
     </div>
-    <hr>
+    <br>
+</div>
+
+<div class="container" style="background-color: snow; border-left: 6px solid lightsalmon;">
+    <div class="row">
+        <div class="col">
+            <h4>Overall!</h4>
+            <p class="text-left"> <hr>
+                From researching these websites I've learned what users will expect when they use a survey creating website.
+
+                Google forms I feel does the survey creating best, The simple layout that I prefer over Zoho and SurveyMonkey, It doesn't try to do a massive range of features, 
+                It's very concise and to the point, It's very simple with a high level of quality. The account creation is easy and it doesn't include any paid features. 
+                The website is beautifully made and a lot of time has clearly been invested in making this finished product.  It doesn't try to restrict what the user can do.
+                <br> <br>
+                Survey Monkey is the second-best creator, It provides much more detail and features than google forms, but loses some of the streamline and simplicity as well. 
+                While it does fill the gaps that google forms leaves, It adopts micro-transactions which in my opinion will turn a lot of users away and towards a free alternative. 
+                Because of the added features, the website does become very confusing and not as intuitive, but they do deploy features such as tutorials and templates to counter this. 
+                The layout is strong but personal I much prefer the look and feel of google forms. 
+                <br> <br>
+                Lastly, Zoho. This was probably the worst out of the research I did. It feels very similar to Survey Monkey without the attention to simplicity and care. 
+                It feels like too much crammed into one space, Lots of questions are given but very little explanation. The layout was confusing and over-saturated, 
+                While it does include some useful features, and questions types, the survey builder layout is not very intuitive. 
+                The overall layout is much more detailed than the other websites, and I feel that the reason I didn't enjoy it as much was that it wasn't targeted towards me, 
+                I think it's targeted towards businesses who want ultimate control and can pay for extra features.
+                <br> <br>
+                Overall, I personally rate them in order of preference of Google Forms, Survey Monkey and Zoho. Having such a range of websites that ultimately do the same task, 
+                has given me a wide scope of what I should aim for and what users will come to expect in a website of this type.
+            </p>  
+        </div>
+    </div>
 </div>
