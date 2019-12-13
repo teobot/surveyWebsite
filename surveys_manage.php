99576fe715bc70a31faa8972214bf51dd3548d06
teobot <theoclapperton@outlook.com>
2019-12-13 03:26:03 +0000

    added ability to search by survey title

--- surveys_manage.php
+++ surveys_manage.php
@@ -22,20 +22,23 @@ if (!isset($_SESSION['loggedIn']))
 // the user must be signed-in, show them suitable page content
 else
 {
-	echo '<a class="btn btn-success btn-lg btn-block" href="survey_Creator.php" role="button">+ Click to create new survey!</a><br>';
-	echo<<<_END
-	<div style="border-left: 5px solid lightblue;" class="container">
-		<input id="searchForUser" class="form-control mr-sm-2" type="search" placeholder="Search By Survey Title"><br>
+	// Making a API call to the returnUsers.php API, asking for all the usernames in the database,
+	// And also posting the current username of the logged in user to make sure they're a admin.
+	$username = $_SESSION['username'];
 
-		<div id="displaySearchedUser">
+	echo<<<_END
+
+	<a class="btn btn-success btn-lg btn-block" href="survey_Creator.php" role="button">+ Click to create new survey!</a><br>
+
+	<div style="border-left: 5px solid lightblue;" class="container">
+		<input id="searchForSurvey" class="form-control mr-sm-2" type="search" placeholder="Search By Survey Title"><br>
+		<div id="displaySearchedSurvey">
 			<div class="alert alert-info" role="alert">
 				Start Searching For Surveys...
 			</div>	
 		</div>
+	</div><br>
 
-	</div>
-_END;
-	echo<<<_END
 	<div class="table-responsive">
 		<table class="table table-hover table-sm text-center" id="surveyTable">
 			<thead>
@@ -52,17 +55,32 @@ _END;
 			</thead>
 		</table>
 	</div>
-_END;
 
-	// Making a API call to the returnUsers.php API, asking for all the usernames in the database,
-	// And also posting the current username of the logged in user to make sure they're a admin.
-	$username = $_SESSION['username'];
-	echo<<<_END
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 		<script>
 			$(document).ready(function() {	
 				// start checking for updates:
+				var surveyData = [];
 				getSurveys();	
+			});
+
+			$('#searchForSurvey').on('input', function() {
+				var surveyToFind = $(this).val();
+
+				$.each(surveyData, function(index, value) {
+
+					if(surveyToFind === value['survey_title']) {
+
+						document.getElementById("displaySearchedSurvey").innerHTML = '<div class="table-responsive table-sm"><table class="table table-hover table-sm text-center"><thead><tr><th>Survey ID</th><th>Creator</th><th>Survey Title</th><th>Survey Edit</th><th>Survey View</th><th>Analytics</th><th>Responses</th><th>Delete</th></tr></thead><tbody><tr><td>'+value.survey_id+'</td><td>'+value.creator+'</td><td>'+value.survey_title+'</td><td><a href="survey_Creator.php?surveyID='+value.survey_id+'">Edit Survey<a></td><td><a href="survey_view.php?surveyID=' + value.survey_id + '">View Survey</a></td><td><a href="survey_Analysis.php?surveyID='+value.survey_id+'">View Analytics</a></td><td><div class="text-center"><span class="badge badge-primary badge-pill">'+value.responseCount+'</span></div></td><td><button type="button" data-surveyid="'+value.survey_id+'" class="deleteSurvey btn btn-outline-danger btn-sm">Delete Survey</button></td></tr></tbody></table></div>'
+						return false;
+
+					} else if (surveyToFind === "") {
+						document.getElementById("displaySearchedSurvey").innerHTML = '<div class="alert alert-info" role="alert">Start Searching For Surveys...</div>';
+					} else {
+						document.getElementById("displaySearchedSurvey").innerHTML = '<div class="alert alert-warning" role="alert">Searched Survey Does Not Exist!</div>';
+					}
+
+				});
 			});
 
 			$(document).on('click', '.deleteSurvey', function(){
@@ -78,6 +96,7 @@ _END;
 			function getSurveys() {
 				$.post('assets/api/returnUsersSurveys.php', {username: '$username' })
 					.done(function(data) {
+						surveyData = data;
 						
 						// remove the old table rows:
 						$('.surveys').remove();
